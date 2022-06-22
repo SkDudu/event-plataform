3329425b1be1c916f0ad123c2e92aba9e5d55d28
Eduardo Santos <52334473+SkDudu@users.noreply.github.com>
2022-06-22 19:42:44 -0300

    adicionando sidebar (css)

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,10 @@
-import { gql, useQuery } from '@apollo/client'
-import { useEffect } from 'react'
-
-import "./styles/global.css"
-
-const GET_LESSON_QUERY = gql`
-  query{
-    lessons{
-      id
-      title
-    }
-  }
-`
-
-interface Lesson {
-  id: string;
-  title: string;
-}
+import { Evento } from "./pages/Evento"
 
 function App() {
-  const {data} = useQuery(GET_LESSON_QUERY)
-
-  return (
-    <ul>
-      {data?.lessons.map((lesson: Lesson) => {
-        return <li key={lesson.id}>{lesson.title}</li>
-      })}
-    </ul>
+    return (
+    <div>
+      <Evento/>
+    </div>
   )
 }
 
